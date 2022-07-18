1a6930ec7dda79c068ef189c7be49fb0bd0a8633
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-07-18 16:40:02 +0200

    add javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+let icon = document.querySelector('.fa-solid');
+
+let header = document.querySelector('header');
+
+let menu = document.querySelector('.menu');
+
+
+icon.addEventListener('click',()=>{
+    icon.classList.replace('fa-bars','fa-x');
+    header.classList.add("activeheader");
+    menu.classList.add("activemenu");
+
+
+
+
+})
