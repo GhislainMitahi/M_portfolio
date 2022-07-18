71347577629fbb68d712aa0a20a8b7bc352e0805
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-07-19 01:31:08 +0200

    fixing linters

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let button = document.querySelector('.button');
+
 let navToggle = document.querySelector('.nav');
+
 button.addEventListener('click',()=>{
     button.classList.toggle('active');
     navToggle.classList.toggle("active");
