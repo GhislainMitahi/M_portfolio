394613bb8e7ade05f259f75cb4e04eec42226fac
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-07-19 01:27:46 +0200

    fixing liters issues

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let button = document.querySelector('.button');
-
 let navToggle = document.querySelector('.nav');
-
 button.addEventListener('click',()=>{
     button.classList.toggle('active');
     navToggle.classList.toggle("active");
