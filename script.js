2ddc0831a283eb9c6a9fe462d05be7190b017493
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-07-19 10:24:02 +0200

    improve menu display

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const button = document.querySelector('.button');
 const navToggle = document.querySelector('.nav');
+const link1 = document.querySelector('.link1');
+const link2 = document.querySelector('.link2');
+const link3 = document.querySelector('.link3');
+
+link1.addEventListener('click', () => {
+  button.classList.remove('active');
+  navToggle.classList.remove('active');
+});
+
+link2.addEventListener('click', () => {
+  button.classList.remove('active');
+  navToggle.classList.remove('active');
+});
+
+link3.addEventListener('click', () => {
+  button.classList.remove('active');
+  navToggle.classList.remove('active');
+});
 
 button.addEventListener('click', () => {
   button.classList.toggle('active');
