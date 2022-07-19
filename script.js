d2903a887b77b58c16030be0a34777ae262a0cff
Ghislain Mitahi <ghislainmitahi@gmail.com>
2022-07-19 15:19:54 +0200

    apply requirement details

--- script.js
+++ script.js
@@ -3,23 +3,28 @@ const navToggle = document.querySelector('.nav');
 const link1 = document.querySelector('.link1');
 const link2 = document.querySelector('.link2');
 const link3 = document.querySelector('.link3');
+const headContent = document.querySelector('.head-content');
 
 link1.addEventListener('click', () => {
   button.classList.remove('active');
   navToggle.classList.remove('active');
+  headContent.classList.toggle('active');
 });
 
 link2.addEventListener('click', () => {
   button.classList.remove('active');
   navToggle.classList.remove('active');
+  headContent.classList.toggle('active');
 });
 
 link3.addEventListener('click', () => {
   button.classList.remove('active');
   navToggle.classList.remove('active');
+  headContent.classList.toggle('active');
 });
 
 button.addEventListener('click', () => {
   button.classList.toggle('active');
   navToggle.classList.toggle('active');
+  headContent.classList.toggle('active');
 });
